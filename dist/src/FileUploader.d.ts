f411519ec773326045df1039068d04a060c7869c
M-FINANCE\yammy.wong <yammy.wong@m-finance.net>
2023-02-05 19:18:22 +0800

    add dist folder

---
+++ dist/src/FileUploader.d.ts
@@ -0,0 +1,50 @@
+import React from 'react';
+declare type Props = {
+    name?: string;
+    hoverTitle?: string;
+    types?: Array<string>;
+    classes?: string;
+    children?: JSX.Element;
+    maxSize?: number;
+    minSize?: number;
+    fileOrFiles?: Array<File> | File | null;
+    disabled?: boolean | false;
+    label?: string | undefined;
+    uploadedLabel?: string;
+    multiple?: boolean | false;
+    maxSizeErrorMsg?: string | undefined;
+    minSizeErrorMsg?: string | undefined;
+    typeErrorMsg?: string | undefined;
+    onSizeError?: (arg0: string | undefined) => void;
+    onTypeError?: (arg0: string | undefined) => void;
+    onDrop?: (arg0: File | Array<File>) => void;
+    onSelect?: (arg0: File | Array<File>) => void;
+    handleChange?: (arg0: File | Array<File> | File) => void;
+    onDraggingStateChange?: (dragging: boolean) => void;
+    dropMessageStyle?: React.CSSProperties | undefined;
+};
+/**
+ * File uploading main function
+ * @param props - {name,
+    hoverTitle,
+    types,
+    handleChange,
+    classes,
+    children,
+    maxSize,
+    minSize,
+    fileOrFiles,
+    onSizeError,
+    onTypeError,
+    onSelect,
+    onDrop,
+    onTypeError,
+    disabled,
+    label,
+    multiple,
+    onDraggingStateChange
+  }
+ * @returns JSX Element
+ */
+declare const FileUploader: React.FC<Props>;
+export default FileUploader;
